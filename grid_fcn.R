552cf2aef9f02d1fbde7e9f1b04e8031bc65a522
Alberto Garcia <agarcia@esm.ucsb.edu>
2019-05-26 00:58:15 -0700

    aggregated to grid level

--- grid_fcn.R
+++ grid_fcn.R
@@ -5,7 +5,9 @@ library(ggplot2)
 library(rlist)
 library(tidyverse)
 
-# grid_fcn <- function(defor_df, celllsize) {
+cellsize = 11
+
+# grid_fcn <- function(defor_df, cellsize){
 
  
 
@@ -14,7 +16,7 @@ library(tidyverse)
 rootn <- ceiling(sqrt(nobs*2))
 landscape = st_sfc(st_polygon(list(cbind(c(0,rootn,rootn,0,0),c(0,0,rootn,rootn,0)))))
 
-overgrid <- st_make_grid(landscape, cellsize = 11, square = TRUE)
+overgrid <- st_make_grid(landscape, cellsize, square = TRUE)
 
 #trim grid to landscape
 overgrid <- st_intersection(overgrid, landscape)
@@ -25,10 +27,6 @@ treat_grids <- list.sample(overgrid, round(length(overgrid)/2))
 # calculate untreated gridcells
 untreat_grids <- overgrid[!overgrid %in% treat_grids]
 
-# could plot treated cells over landscape
-# plot(landscape, col = "#339900")
-# plot(overgrid, axes = TRUE, add = TRUE)
-# plot(treat_grids, col = "red", add = TRUE)
 
 #### need to assign units location in treated or untreatedd gridcells
 
@@ -37,16 +35,40 @@ treat_locs <- st_sample(treat_grids, nobs)
 untreat_locs <- st_sample(untreat_grids, nobs)
 
 
-# plot(landscape, col = "#339900")
-# plot(treat_grids, axes = TRUE, add = TRUE)
-# plot(treat_locs, col = "blue", add = TRUE)
-# plot(untreat_locs, col = "black", add = TRUE)
+treat_counties <- st_within(treat_locs, treat_grids, sparse = FALSE, prepared = TRUE)*1
+untreat_counties <- st_within(untreat_locs, untreat_grids, sparse = FALSE, prepared = TRUE)*1
+t_whichcounty <- max.col(treat_counties)
+u_whichcounty <- max.col(untreat_counties)
+
+#### now we need to assign the random locations to the pixels in defor_df
+# separating treated and untreated units
+defor_df$treat <- as.numeric(defor_df$treat)
+df_tr <- subset(defor_df, treat ==1)
+df_un <- subset(defor_df, treat ==0)
+
+# assigning each unique unit a location
+unique_un <- unique(df_un$idx)
+unique_tr <- unique(df_tr$idx)
+df_tr <- st_sf(unique_un, u_whichcounty, geometry = untreat_locs)
+df_un <- st_sf(unique_tr, t_whichcounty, geometry = treat_locs)
+names(df_un)[2] <- paste("county")
+names(df_tr)[2] <- paste("county")
+colnames(df_tr)[1] <- "idx"
+colnames(df_un)[1] <- "idx"
+df_3 <- rbind(df_tr, df_un)
+
+#matching coordinates back to original dataframe
+
+df_match <-  merge(df_3, defor_df, by = "idx")
+
+
+### calculate average defor rate in each county based on year
+# aggregate up to county in each year 
+suppressWarnings(
+  countylevel_df <-  aggregate(df_match, by = list(df_match$county, df_match$treat, df_match$year), FUN = mean, drop = TRUE)[c("county", "treat", "year","defor")]
+)
 
 
 
-
-
-
-
-
+# }
 
