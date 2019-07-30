8f4513bc88b29c2d7486b9ce062536b4fc4a1d2b
Alberto Garcia <agarcia@esm.ucsb.edu>
2019-07-30 09:17:58 -0700

    testing new folder arrangements

--- agg_coeffdist_fcn.R
+++ agg_coeffdist_fcn.R
@@ -7,22 +7,17 @@ library(plm)
 library(IDPmisc)
 library(tictoc)
 #begin function
-agg_coeffdist_fcn <- function(n, nobs, years, psize, cellsize){
+agg_coeffdist_fcn <- function(n, nobs, years, psize, cellsize, std_p, att){
   
-  #preallocate n x _ matrix
-  coeffmatrix <- matrix(nrow = n, ncol = 14)
-  
+  #preallocate n x 2 matrix
+  coeffmatrix <- matrix(nrow = n, ncol = 3)
+  areamatrix <- matrix(nrow = n, ncol = 2)
   for(i in 1:n){
     
     tic("loop")
     # call defor_sim function to simulate dataframe, returned as defor_df  
-    prop_deforsim(nobs, years, psize, cellsize)  
-    #prop_deforsim(10000,3,1200,25)  
-    ############### gather area for each property and county ##################
+    prop_deforsim(nobs, years, psize, cellsize, std_p, att)  
     
-    
-    
-
     # lagging deforestation and forest share variable
 
     countypert_df <- countypert_df[order(countypert_df$countyid, countypert_df$year),]
@@ -39,44 +34,15 @@ agg_coeffdist_fcn <- function(n, nobs, years, psize, cellsize){
     countypert_df$forsharelag <- 1 -  countypert_df$deforlag
     proppert_df$forsharelag <- 1 - proppert_df$deforlag
     
-    #### create baseline defor and forshare vars 
-    #county level
-    year1 <- subset(countypert_df, year== 1)
-    colnames(year1)[colnames(year1)=="defor"] <- "defor0"
-    year1 <- subset(year1, select = c(countyid, defor0))
-    #st_geometry(year1) <- NULL
-    countypert_df <-  merge(countypert_df, year1, by = "countyid")
-    #property level
-    year1 <- subset(proppert_df, year== 1)
-    colnames(year1)[colnames(year1)=="defor"] <- "defor0"
-    year1 <- subset(year1, select = c(property, defor0))
-    #st_geometry(year1) <- NULL
-    proppert_df <-  merge(proppert_df, year1, by = "property")
-    countypert_df$forshare0 <- 1 -  countypert_df$defor0
-    proppert_df$forshare0 <- 1 - proppert_df$defor0
+    ##### constructing outcome variables #####
+    proppert_df$forcovariate <- log(proppert_df$forshare * proppert_df$parea)
+    
+    proppert_df$deforrate1 <- ((proppert_df$forsharelag - proppert_df$forshare) / proppert_df$forsharelag)
     
     
-    ##### constructing various outcome variables #####
+    countypert_df$deforrate1 <- ((countypert_df$forsharelag- countypert_df$forshare) / countypert_df$forsharelag)
     
-    
-    countypert_df$deforrate1 <- countypert_df$defor / countypert_df$defor0
-    proppert_df$deforrate1 <- proppert_df$defor / proppert_df$defor0
-    
-    countypert_df$deforrate2 <- countypert_df$defor / countypert_df$deforlag
-    proppert_df$deforrate2 <- proppert_df$defor / proppert_df$deforlag
-    
-    countypert_df$deforrate3 <- countypert_df$defor / countypert_df$forshare0
-    proppert_df$deforrate3 <- proppert_df$defor / proppert_df$forshare0
-    
-    countypert_df$deforrate4 <- countypert_df$defor / countypert_df$forsharelag
-    proppert_df$deforrate4 <- proppert_df$defor / proppert_df$forsharelag
-    
-    countypert_df$deforrate6 <- countypert_df$forshare / countypert_df$forshare0
-    proppert_df$deforrate6 <- proppert_df$forshare / proppert_df$forshare0
-    
-    countypert_df$deforrate7 <- countypert_df$forshare / countypert_df$forsharelag
-    proppert_df$deforrate7 <- proppert_df$forshare / proppert_df$forsharelag
-    
+    proppert_df$deforrate2 <- log(1+ proppert_df$defor*proppert_df$parea)
     #remove infinite values
 
     proppert_df <- subset(proppert_df, select = -c(geometry))
@@ -88,157 +54,48 @@ agg_coeffdist_fcn <- function(n, nobs, years, psize, cellsize){
       countypert_df %>% 
       filter_all(all_vars(!is.infinite(.)))
     
-    # run two-way fixed effects with propertylevel data
+    
+    # run two-way fixed effects with county level data
     coeffmatrix[i,1] <- plm(deforrate1 ~  post*treat, 
-                            data   = proppert_df, 
+                            data   = countypert_df, 
+                            weights=(countypert_df$carea),
                             method = "within", #fixed effects model
-                            effect = "twoway", #property and year fixed effects
-                            index  = c("property", "year")
-    )$coefficients
+                            effect = "twoway", #county and year fixed effects
+                            index  = c("countyid", "year")
+    )$coefficients - att
     
-    # run two-way fixed effects with county level data
+    # run two-way fixed effects with propertylevel data
     coeffmatrix[i,2] <- plm(deforrate1 ~  post*treat, 
-                            data   = countypert_df, 
-                            method = "within", #fixed effects model
-                            effect = "twoway", #county and year fixed effects
-                            index  = c("countyid", "year")
-    )$coefficients
-    
-    
-    # # run two-way fixed effects with propertylevel data
-    # coeffmatrix[i,3] <- plm(deforrate2 ~  post*treat, 
-    #                         data   = proppert_df, 
-    #                         method = "within", #fixed effects model
-    #                         effect = "twoway", #property and year fixed effects
-    #                         index  = c("property", "year")
-    # )$coefficients
-    # 
-    # # run two-way fixed effects with county level data
-    # coeffmatrix[i,4] <- plm(deforrate2 ~  post*treat, 
-    #                         data   = countypert_df, 
-    #                         method = "within", #fixed effects model
-    #                         effect = "twoway", #county and year fixed effects
-    #                         index  = c("countyid", "year")
-    # )$coefficients
-    # 
-    
-    coeffmatrix[i,3]  <- lm(deforrate2 ~  post*treat, 
-                            data = countypert_df
-    )$coefficients[4]
-    
-    coeffmatrix[i,4]  <- lm(deforrate2 ~  post*treat, 
-                            data = proppert_df
-    )$coefficients[4]
-    
-    
-    
-    # run two-way fixed effects with propertylevel data
-    coeffmatrix[i,5] <- plm(deforrate3 ~  post*treat, 
                             data   = proppert_df, 
+                            weights=(proppert_df$parea),
                             method = "within", #fixed effects model
                             effect = "twoway", #property and year fixed effects
                             index  = c("property", "year")
-    )$coefficients
-    
-    # run two-way fixed effects with county level data
-    coeffmatrix[i,6] <- plm(deforrate3 ~  post*treat, 
-                            data   = countypert_df, 
-                            method = "within", #fixed effects model
-                            effect = "twoway", #county and year fixed effects
-                            index  = c("countyid", "year")
-    )$coefficients
-
- 
-      # run two-way fixed effects with propertylevel data
-      # coeffmatrix[i,7] <- plm(deforrate4 ~  post*treat, 
-      #                         data   = proppert_df, 
-      #                         method = "within", #fixed effects model
-      #                         effect = "twoway", #property and year fixed effects
-      #                         index  = c("property", "year")
-      # )$coefficients
-    
-    # run two-way fixed effects with county level data
-    # coeffmatrix[i,8] <- plm(deforrate4 ~  post*treat, 
-    #                         data   = countypert_df, 
-    #                         method = "within", #fixed effects model
-    #                         effect = "twoway", #county and year fixed effects
-    #                         index  = c("countyid", "year")
-    # )$coefficients
-    
-    coeffmatrix[i,7]  <- lm(deforrate4 ~  post*treat, 
-                            data = countypert_df
-    )$coefficients[4]
-    
-    coeffmatrix[i,8]  <- lm(deforrate4 ~  post*treat, 
-                            data = proppert_df
-    )$coefficients[4]
-    
-    ######################### Busch et al (2014): (f_it-1 -f_it)/f_it-1  ########################### 
-    ### creating deforrate5 outcome variable for both dataframes
-    
-    proppert_df$deforrate5 <- ((proppert_df$forsharelag - proppert_df$forshare) / proppert_df$forsharelag)
+    )$coefficients - att
     
     
-    countypert_df$deforrate5 <- ((countypert_df$forsharelag- countypert_df$forshare) / countypert_df$forsharelag)
-    
-      
-      # run two-way fixed effects with propertylevel data
-      coeffmatrix[i,9] <- plm(deforrate5 ~  post*treat, 
-                              data   = proppert_df, 
-                              method = "within", #fixed effects model
-                              effect = "twoway", #property and year fixed effects
-                              index  = c("property", "year")
-      )$coefficients
-    
-    # run two-way fixed effects with county level data
-    coeffmatrix[i,10] <- plm(deforrate5 ~  post*treat, 
-                            data   = countypert_df, 
-                            method = "within", #fixed effects model
-                            effect = "twoway", #county and year fixed effects
-                            index  = c("countyid", "year")
-    )$coefficients
-    
-    # run two-way fixed effects with propertylevel data
-    coeffmatrix[i,11] <- plm(deforrate6 ~  post*treat, 
+    coeffmatrix[i,3] <- plm(deforrate2 ~  post*treat + forcovariate, 
                             data   = proppert_df, 
+                            weights=(proppert_df$parea),
                             method = "within", #fixed effects model
                             effect = "twoway", #property and year fixed effects
                             index  = c("property", "year")
-    )$coefficients
+    )$coefficients - att
     
-    # run two-way fixed effects with county level data
-    coeffmatrix[i,12] <- plm(deforrate6 ~  post*treat, 
-                             data   = countypert_df, 
-                             method = "within", #fixed effects model
-                             effect = "twoway", #county and year fixed effects
-                             index  = c("countyid", "year")
-    )$coefficients
-    
-    # run two-way fixed effects with propertylevel data
-    coeffmatrix[i,13] <- plm(deforrate7 ~  post*treat, 
-                            data   = proppert_df, 
-                            method = "within", #fixed effects model
-                            effect = "twoway", #property and year fixed effects
-                            index  = c("property", "year")
-    )$coefficients
-    
-    # run two-way fixed effects with county level data
-    coeffmatrix[i,14] <- plm(deforrate7 ~  post*treat, 
-                             data   = countypert_df, 
-                             method = "within", #fixed effects model
-                             effect = "twoway", #county and year fixed effects
-                             index  = c("countyid", "year")
-    )$coefficients
+    areamatrix[i,1] <- mean(countypert_df$carea)
+    areamatrix[i,2]<- mean(proppert_df$parea)
     
     toc()
-    rm(year1, countypert_df, proppert_df)
+    rm(countypert_df, proppert_df)
     #end for loop  
   }  
   
   # get distribution information from matrix  
+  assign('coeff_agg',coeffmatrix, envir=.GlobalEnv)
+  assign('areamatrix',areamatrix, envir=.GlobalEnv)
   return(list(colMeans(coeffmatrix), colVars(coeffmatrix)))
   
-  assign('coeffmatrix',coeffmatrix, envir=.GlobalEnv)
+  
   
   
   #end function  
