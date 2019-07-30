d614e68390102143cc94996c04b680e866c3b106
Alberto Garcia <agarcia@esm.ucsb.edu>
2019-07-30 09:15:53 -0700

    implemented new dgp

--- binary_coeffdist_fcn.R
+++ binary_coeffdist_fcn.R
@@ -1,10 +1,12 @@
 # this function's intention is to provide distributional parameters for the various specifications that have the typical binary outcome variable
 library(clubSandwich)
 library(matrixStats)
-
-
+library(ggplot2)
+library(plm)
+library(Metrics)
 #begin function
-binary_coeffdist_fcn <- function(n, nobs, years){
+
+binary_coeffdist_fcn <- function(n, nobs, years, b1,b2,b3){
   
   #preallocate n x 4 matrix
   coeffmatrix <- matrix(nrow = n, ncol = 4)
@@ -13,51 +15,64 @@ binary_coeffdist_fcn <- function(n, nobs, years){
   
   
     # call defor_sim function to simulate dataframe, returned as defor_df  
-    defor_sim(nobs, years)  
+    defor_DGP(nobs, years, b1, b2, b3)
 
-   
     # run DID dropping deforested pixels
-    coeffmatrix[i,1]  <- lm(defor ~  post*treat, 
-                   data = defor_df
-    )$coefficients[4]
+    coeffmatrix[i,1]  <- lm(y_it ~  post*treat, 
+                   data = panels
+    )$coefficients[4] - ATT
     
     # run two-way fixed effects    
-    coeffmatrix[i, 2] <- plm(defor ~  post*treat, 
-                     data   = defor_df, 
+    coeffmatrix[i, 2] <- plm(y_it ~  post*treat, 
+                     data   = panels, 
                      method = "within", #fixed effects model
                      effect = "twoway", #unit and year fixed effects
-                     index  = c("idx", "year")
-    )$coefficients
+                     index  = c("pixels", "year")
+    )$coefficients - ATT
     
-    # turn dropped outcome observations to 1
-    defor_df$defor[is.na(defor_df$defor)] <- 1
     
     # DID keeping variables
     
     coeffmatrix[i, 3] <- lm(defor ~  post*treat, 
-                           data = defor_df
-    )$coefficients[4]
+                           data = panels
+    )$coefficients[4] - ATT
     
     # run two-way fixed effects    
     coeffmatrix[i, 4] <- plm(defor ~  post*treat, 
-                       data   = defor_df, 
+                       data   = panels, 
                        method = "within", #fixed effects model
                        effect = "twoway", #unit and year fixed effects
-                       index  = c("idx", "year")
-    )$coefficients
+                       index  = c("pixels", "year")
+    )$coefficients - ATT
     
-    rm(defor_df)
     
   #end for loop  
   }  
   assign('coeff_bin',coeffmatrix, envir=.GlobalEnv)
   
 # get distribution information from matrix  
-  return(list(colMeans(coeffmatrix), colVars(coeffmatrix)))
+
+  b_coeff <- as.data.frame(coeff_bin)
   
+  actual <- rep(ATT, times = n)
+  
+  p1 <- ggplot() +
+    geom_density(data = b_coeff , aes(x = V1), alpha = .2, fill="#29CD44") +
+    geom_density(data = b_coeff , aes(x = V2), alpha = .2, fill="#FF6655") +
+    geom_density(data = b_coeff , aes(x = V3), alpha = .2, fill="#0000CC") +
+    geom_density(data = b_coeff , aes(x = V4), alpha = .2, fill="#0000CC") +
+    geom_vline(data = b_coeff, xintercept = mean(b_coeff$V1),linetype = "dashed")+
+    geom_vline(data = b_coeff, xintercept = mean(b_coeff$V2),linetype = "dashed")+
+    geom_vline(data = b_coeff, xintercept = mean(b_coeff$V3),linetype = "dashed")+
+    theme(plot.caption = element_text(hjust = 0.5))+
+    labs(x= "Bias", title = "Bias with binary outcome", caption = paste("The mean and RMSE are:", "\n", "DID dropping obs:", round(colMeans(coeff_bin)[1], digits = 4),"RMSE:",round(rmse(actual, coeff_bin[1]), digits = 5), "\n", 
+                                                                        "2way FE dropping obs:", round(colMeans(coeff_bin)[2], digits = 4), "RMSE:",round(rmse(actual, coeff_bin[2]), digits = 5), "\n",  
+                                                                        "DID keeping obs:",round(colMeans(coeff_bin)[3], digits = 4) ,"RMSE:",round(rmse(actual, coeff_bin[3]), digits = 5),  "\n", 
+                                                                        "2way FE keeping obs:",round(colMeans(coeff_bin)[4], digits = 4) ,"RMSE:",round(rmse(actual, coeff_bin[4]), digits = 5)))
+  p1
 #end function  
 }  
-  
+
   
   
   
