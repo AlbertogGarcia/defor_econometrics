d3ca0874ce7c8f2796819510e55abc7ad267d23d
Alberto Garcia <agarcia@esm.ucsb.edu>
2019-08-01 11:21:18 -0700

    adjusting to new dgp

--- finaldgp/DID_bias.R
+++ finaldgp/DID_bias.R
@@ -7,35 +7,38 @@ DID_bias <- function(n, nobs, years, att){
   
   for(i in 1:n){
     
-    diff1 <- .2
-    trend1 <- .1
-    diff2 <- -diff1
-    trend2 <- -trend1  
-    # call defor_sim function to simulate dataframe, returned as defor_df  
-    defor_sim(nobs, years, att, diff1, trend1)
+   b0 <- .3
+   b1p <- .2
+   b1n <- -b1p
+   b2p <- -.1
+   b2n <- -b2p
+    
+     
+    # call defor_dgp function to simulate dataframe, returned as defor_df  
+    defor_dgp(nobs, years, b0, b1p, b2p)
     
     # run DID dropping deforested pixels
-    coeffmatrix[i,1]  <- lm(defor ~  post*treat, 
+    coeffmatrix[i,1]  <- lm(y_it ~  post*treat, 
                             data = defor_df
-    )$coefficients[4] - att
+    )$coefficients[4] - ATT
    
     
-    defor_sim(nobs, years, att, diff1, trend2)
-    coeffmatrix[i,2]  <- lm(defor ~  post*treat, 
+    defor_dgp(nobs, years, b0, b1p, b2n)
+    coeffmatrix[i,2]  <- lm(y_it ~  post*treat, 
                             data = defor_df
-    )$coefficients[4] - att
+    )$coefficients[4] - ATT
   
     
-    defor_sim(nobs, years, att, diff2, trend1)
-    coeffmatrix[i,3]  <- lm(defor ~  post*treat, 
+    defor_dgp(nobs, years, b0, b1n, b2p)
+    coeffmatrix[i,3]  <- lm(y_it ~  post*treat, 
                             data = defor_df
-    )$coefficients[4] - att
+    )$coefficients[4] - ATT
     
     
-    defor_sim(nobs, years, att, diff2, trend2)
-    coeffmatrix[i,4]  <- lm(defor ~  post*treat, 
+    defor_dgp(nobs, years, b0, b1n, b2n)
+    coeffmatrix[i,4]  <- lm(y_it ~  post*treat, 
                             data = defor_df
-    )$coefficients[4] - att
+    )$coefficients[4] - ATT
     
     print(i)
     
@@ -53,11 +56,11 @@ DID_bias <- function(n, nobs, years, att){
   pdid <- ggplot(data = did_coeff, aes(x = bias, fill=variable)) +
     geom_density(alpha = .2) +
     guides(fill=guide_legend(title=NULL))+
-    scale_fill_discrete(breaks=c("++", "+-", "-+", "--"), labels=c("diff+, trend+", "diff+, trend-", "diff-, trend+", "diff-, trend-"))+
+    scale_fill_discrete(breaks=c("++", "+-", "-+", "--"), labels=c("b1+, b2+", "b1+, b2-", "b1-, b2+", "b1-, b2-"))+
     geom_vline(xintercept = 0, linetype = "dashed")+
     #theme(plot.margin = unit(c(1,1,3,1), "cm"))+
     theme(plot.caption = element_text(hjust = 0.5))+
-    labs(x= "bias", title = "DID bias with binary outcome", caption = paste("ATT = ",att , "for each of the four density plots. n=", n , "\n",  "The mean and variance are:", "\n", "diff+, trend+:", round(colMeans(coeff_didbias)[1], digits = 4),"var:",round(colVars(coeff_didbias)[1], digits = 6), "\n",  "diff+, trend-:", round(colMeans(coeff_didbias)[2], digits = 4), "var:",round(colVars(coeff_didbias)[2], digits = 6), "\n",  "diff-, trend+:",round(colMeans(coeff_didbias)[3], digits = 4) ,"var:",round(colVars(coeff_didbias)[3], digits = 6),  "\n", "diff-, trend+:",round(colMeans(coeff_didbias)[4], digits = 4) ,"var:",round(colVars(coeff_didbias)[4], digits = 6)))
+    #labs(x= "bias", title = "DID bias with binary outcome", caption = paste("ATT = ",att , "for each of the four density plots. n=", n , "\n",  "The mean and variance are:", "\n", "diff+, trend+:", round(colMeans(coeff_didbias)[1], digits = 4),"var:",round(colVars(coeff_didbias)[1], digits = 6), "\n",  "diff+, trend-:", round(colMeans(coeff_didbias)[2], digits = 4), "var:",round(colVars(coeff_didbias)[2], digits = 6), "\n",  "diff-, trend+:",round(colMeans(coeff_didbias)[3], digits = 4) ,"var:",round(colVars(coeff_didbias)[3], digits = 6),  "\n", "diff-, trend+:",round(colMeans(coeff_didbias)[4], digits = 4) ,"var:",round(colVars(coeff_didbias)[4], digits = 6)))
   pdid
   
   
