492b2cc90beb9f3cfb6efae1b2f7569dd5583976
Alberto Garcia <agarcia@esm.ucsb.edu>
2019-08-21 16:07:54 -0700

    bias exploration files

--- finaldgp/DID_bias.R
+++ finaldgp/DID_bias.R
@@ -1,16 +1,15 @@
 library(ggplot2)
 #begin function
-DID_bias <- function(n, nobs, years, b3){
+DID_bias <- function(n, nobs, years, b0, b1p, b2p, b3){
   
   #preallocate n x 4 matrix
   coeffmatrix <- matrix(nrow = n, ncol = 4)
   
   for(i in 1:n){
     
-   b0 <- .25
-   b1p <- .1
+
    b1n <- -b1p
-   b2p <- .1
+
    b2n <- -b2p
     
      
@@ -18,25 +17,25 @@ DID_bias <- function(n, nobs, years, b3){
     defor_DGP(nobs, years, b0, b1p, b2p, b3)
     
     # run DID dropping deforested pixels
-    coeffmatrix[i,1]  <- lm(y_it ~  post*treat, 
+    coeffmatrix[i,1]  <- lm(y ~  post*treat, 
                             data = panels
     )$coefficients[4] - ATT
    
     
     defor_DGP(nobs, years, b0, b1p, b2n, b3)
-    coeffmatrix[i,2]  <- lm(y_it ~  post*treat, 
+    coeffmatrix[i,2]  <- lm(y ~  post*treat, 
                             data = panels
     )$coefficients[4] - ATT
   
     
     defor_DGP(nobs, years, b0, b1n, b2p, b3)
-    coeffmatrix[i,3]  <- lm(y_it ~  post*treat, 
+    coeffmatrix[i,3]  <- lm(y ~  post*treat, 
                             data = panels
     )$coefficients[4] - ATT
     
     
     defor_DGP(nobs, years, b0, b1n, b2n, b3)
-    coeffmatrix[i,4]  <- lm(y_it ~  post*treat, 
+    coeffmatrix[i,4]  <- lm(y ~  post*treat, 
                             data = panels
     )$coefficients[4] - ATT
     
---
+++ finaldgp/quickmonteyit.R
@@ -0,0 +1,47 @@
+library(ggplot2)
+library(clubSandwich)
+library(matrixStats)
+library(ggplot2)
+library(plm)
+library(Metrics)
+#begin function
+quickmonteyit <- function(n, nobs, years, b0, b1, b2, b3){
+  
+  #preallocate n x 4 matrix
+  coeffmatrix <- matrix(nrow = n, ncol = 1)
+  
+  for(i in 1:n){
+    
+    
+    
+    # call defor_dgp function to simulate dataframe, returned as defor_df  
+    defor_DGP(nobs, years, b0, b1, b2, b3)
+    
+    # run DID dropping deforested pixels
+    coeffmatrix[i,1]  <- lm(y_it ~  post*treat, 
+                            data = panels
+    )$coefficients[4] - ATT
+    
+    print(i)
+    
+    #end for loop  
+  }  
+  
+  #assign('coeff_didbias',coeffmatrix, envir=.GlobalEnv)
+  
+  did_coeff <- as.data.frame(coeffmatrix)
+  actual <- rep(ATT, times = n)
+  
+  ggplot() +
+    geom_density(data = did_coeff , aes(x = V1), alpha = .2, fill="red")+
+    geom_vline(data = did_coeff, xintercept = mean(did_coeff$V1), color = 'red')+
+    geom_vline(data = did_coeff, xintercept = 0, linetype = "dashed")+
+    theme(plot.caption = element_text(hjust = 0.5))+
+    labs(x= "Bias", caption = paste("Mean:", round(mean(did_coeff$V1), digits = 4),
+                                    ", RMSE:", round(rmse(actual, did_coeff$V1), digits = 4)) 
+    )
+  
+  
+  
+  #end function  
+}  
