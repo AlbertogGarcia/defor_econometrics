e924bb6f7debec2bb1563d86ea2d5ae15250c9f8
albertoggarcia <garcia.albertg@gmail.com>
2021-01-11 12:03:06 -0800

    implementing county level unobservables

--- unbiased_dgp/layerprocess_fcn.R
+++ unbiased_dgp/layerprocess_fcn.R
@@ -15,16 +15,16 @@ library(fixest)
 source('full_landscape.R')
 
 #begin function
-aggregation_fcn <- function(n, nobs, years, b0, b1, b2_0, b2_1, b3, std_a = 0.1, std_v = 0.25, std_p = 0.0, cellsize, ppoints, cpoints){
+layerprocess_fcn <- function(n, nobs, years, b0, b1, b2_0, b2_1, b3, std_a = 0.1, std_v = 0.25, std_p = 0.0, std_c = 0.0, cellsize, ppoints, cpoints){
   
   countyscape = full_landscape(nobs, cellsize, ppoints, cpoints)
   pixloc_df = countyscape$pixloc_df
   
-  ATT <- pnorm(b0+b1+b2_1+b3, 0, (std_a^2+std_v^2 +std_p^2)^.5) - pnorm(b0+b1+b2_1, 0, (std_a^2+std_v^2 + std_p^2)^.5)
+  ATT <- pnorm(b0+b1+b2_1+b3, 0, (std_a^2+std_v^2 +std_p^2+std_c^2)^.5) - pnorm(b0+b1+b2_1, 0, (std_a^2+std_v^2 + std_p^2+std_c^2)^.5)
   
   pixloc <- pixloc_df
   
-  covermat <- matrix(nrow = n, ncol = 10)
+  covermat <- matrix(nrow = n, ncol = 6)
   coeffmatrix <- matrix(nrow = n, ncol = 4)
   
   for(i in 1:n){
@@ -43,15 +43,15 @@ aggregation_fcn <- function(n, nobs, years, b0, b1, b2_0, b2_1, b3, std_a = 0.1,
     
     #generate random 
     errortable_property <- data.frame(property = as.character(unique(pixloc$property)), p_err = rnorm(length(unique(pixloc$property)), 0, std_p))
-    #errortable_county <- data.frame(county = as.character(unique(pixloc$county)), c_err = rnorm(length(unique(pixloc$county)), 0, std_c))
+    errortable_county <- data.frame(county = as.character(unique(pixloc$county)), c_err = rnorm(length(unique(pixloc$county)), 0, std_c))
     
     panels$pixels <- gsub("(?<![0-9])0+", "", panels$pixels, perl = TRUE)
     
     panels <- panels %>%
       inner_join(pixloc, by = c("pixels", "treat")) %>%
       inner_join(errortable_property, by = "property") %>%
-      #inner_join(errortable_county, by = "county") %>%
-      mutate(ystar = ystar + p_err) %>%
+      inner_join(errortable_county, by = "county") %>%
+      mutate(ystar = ystar + p_err + c_err) %>%
       mutate(y = (ystar > 0)*1 )
     
     #need to determine which year deforestation occurred
@@ -197,13 +197,7 @@ aggregation_fcn <- function(n, nobs, years, b0, b1, b2_0, b2_1, b3, std_a = 0.1,
     coeffmatrix[i,3] <- DID3$coefficients - ATT
     coeffmatrix[i,4] <- DID4$coefficients[4] - ATT
     
-    # classical standard errors
-    se1 <- summary(DID1, se = "hetero")$se[1]
-    se2 <- summary(DID2, se = "hetero")$se[1]
-    se3 <- summary(DID3, se = "hetero")$se[1]
-    se4 <- summary(DID4, se = "hetero")$se[4]
-    
-    #clustering at group level for aggragated analyses
+    #clustering at group level for aggregated analyses
     cluster_se1    <- summary(DID1, cluster = ~grid)$se[1]
     cluster_se2    <- summary(DID2, cluster = ~property)$se[1]
     cluster_se3    <- summary(DID3, cluster = ~county)$se[1]
@@ -220,16 +214,10 @@ aggregation_fcn <- function(n, nobs, years, b0, b1, b2_0, b2_1, b3, std_a = 0.1,
     covermat[i,2] <- between(ATT, DID2$coefficients - 1.96 * cluster_se2, DID2$coefficients + 1.96 * cluster_se2)*1
     covermat[i,3] <- between(ATT, DID3$coefficients - 1.96 * cluster_se3, DID3$coefficients + 1.96 * cluster_se3)*1
     
-    # classical standard errors
-    covermat[i,4] <- between(ATT, DID1$coefficients - 1.96 * se1, DID1$coefficients + 1.96 * se1)*1
-    covermat[i,5] <- between(ATT, DID2$coefficients - 1.96 * se2, DID2$coefficients + 1.96 * se2)*1
-    covermat[i,6] <- between(ATT, DID3$coefficients - 1.96 * se3, DID3$coefficients + 1.96 * se3)*1
-    covermat[i,7] <- between(ATT, DID4$coefficients[4] - 1.96 * se4, DID4$coefficients[4] + 1.96 * se4)*1
-    
     # coverage with pixel level analyses clustered at different levels
-    covermat[i,8] <- between(ATT, DID4$coefficients[4] - 1.96 * cluster_prop_pix, DID4$coefficients[4] + 1.96 * cluster_prop_pix)*1
-    covermat[i,9] <- between(ATT, DID4$coefficients[4] - 1.96 * cluster_grid_pix, DID4$coefficients[4] + 1.96 * cluster_grid_pix)*1
-    covermat[i,10] <- between(ATT, DID4$coefficients[4] - 1.96 * cluster_county_pix, DID4$coefficients[4] + 1.96 * cluster_county_pix)*1
+    covermat[i,4] <- between(ATT, DID4$coefficients[4] - 1.96 * cluster_prop_pix, DID4$coefficients[4] + 1.96 * cluster_prop_pix)*1
+    covermat[i,5] <- between(ATT, DID4$coefficients[4] - 1.96 * cluster_grid_pix, DID4$coefficients[4] + 1.96 * cluster_grid_pix)*1
+    covermat[i,6] <- between(ATT, DID4$coefficients[4] - 1.96 * cluster_county_pix, DID4$coefficients[4] + 1.96 * cluster_county_pix)*1
     
     print(i)
     toc()
@@ -241,36 +229,11 @@ aggregation_fcn <- function(n, nobs, years, b0, b1, b2_0, b2_1, b3, std_a = 0.1,
   names(coeff_bias)[2] <- paste("property")
   names(coeff_bias)[3] <- paste("county")
   names(coeff_bias)[4] <- paste("pixel")
-  suppressWarnings(cbias <- melt(coeff_bias, value.name = "bias"))
   
-  ##################################################################
-  ###### Bias distribution Plots #######
-  
-  plot <- ggplot(data = cbias, aes(x = bias, fill=variable)) +
-    geom_density(alpha = .2) +
-    guides(fill=guide_legend(title=NULL))+
-    scale_fill_discrete(breaks=c("grid", "property", "county", "pixel"), labels=c("aggregated to grids", "aggregated to properties", "aggregated to counties", "pixel"))+
-    geom_vline(xintercept = 0, linetype = "dashed")+
-    #geom_vline(aes(xintercept= (DID_estimand - ATT), color="DID estimand - ATT"), linetype="dashed")+
-    #theme(plot.margin = unit(c(1,1,3,1), "cm"))+
-    theme(plot.caption = element_text(hjust = 0.5))+
-    labs(x= "Bias", caption = paste("Mean grid:", round(mean(coeff_bias$grid), digits = 4),
-                                    ", RMSE:", round(rmse(actual, coeff_bias$grid), digits = 4), "\n", 
-                                    "Mean property:", round(mean(coeff_bias$property), digits = 4),
-                                    ", RMSE:", round(rmse(actual, coeff_bias$property), digits = 4),"\n",
-                                    "Mean county:", round(mean(coeff_bias$county), digits = 4),
-                                    ", RMSE:", round(rmse(actual, coeff_bias$county), digits = 4), "\n",
-                                    "Mean pixel:", round(mean(coeff_bias$pixel), digits = 4),
-                                    ", RMSE:", round(rmse(actual, coeff_bias$pixel), digits = 4)) 
-    )
   
   aggregation <- c('grid', 
                    'property',
                    'county',
-                   'grid',
-                   'property',
-                   'county',
-                   'pixel',
                    'pixel',
                    'pixel',
                    'pixel'
@@ -279,13 +242,9 @@ aggregation_fcn <- function(n, nobs, years, b0, b1, b2_0, b2_1, b3, std_a = 0.1,
   std_error <- c('clustered at grid', 
                  'clustered at property',
                  'clustered at county',
-                 'classical',
-                 'classical',
-                 'classical',
-                 'classical'
-                 , 'clustered at property',
-                 'clustered at grid'
-                 , 'clustered at county'
+                 'clustered at property',
+                 'clustered at grid',
+                 'clustered at county'
   )
   
   coverages_df <- data.frame(aggregation, std_error, coverage = colMeans(covermat)) 
