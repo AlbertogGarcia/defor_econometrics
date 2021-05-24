8e1374744f5617f81790a7bbe10acdf2d5d9745e
albertoggarcia <garcia.albertg@gmail.com>
2021-05-24 09:29:13 -0700

    csvs were updated

--- unbiased_dgp/full_landscape.R
+++ unbiased_dgp/full_landscape.R
@@ -42,8 +42,8 @@ full_landscape <- function(nobs, cellsize, ppoints, cpoints){
     st_geometry() %>% #  as geometry only 
     st_union() %>% # unite them 
     st_voronoi() %>% # perform the voronoi tessellation
-    st_collection_extract(type = "POLYGON") #%>% # select the polygons
-    #st_intersection(v_county)  # limit to within county boundaries
+    st_collection_extract(type = "POLYGON") %>% # select the polygons
+    st_intersection(landscape)  # limit to within landscape boundaries
     
   
   #determine which pixels are in each grid 
